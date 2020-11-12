8c28018f5ec26f48267c29aaa39e919702e16811
LazareC <lazarela17@gmail.com>
2020-11-12 14:26:57 +0100

    Create downloader_for_claroline.user.js

---
+++ downloader_for_claroline.user.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Downloader for Claroline
+// @version      1.0.1
+// @description  Add button to download all pdf on Claroline.
+// @iconURL      https://i.imgur.com/1Cplnzw.png
+// @icon64URL    https://i.imgur.com/1Cplnzw.png
+// @author       LazareC
+// @website      https://lazarec.fr
+// @match        http*://clarolineconnect.univ-lyon1.fr/resource/open/file/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @run-at       document-body
+// @downloadURL  http://lazarec.fr/downloader_for_claroline.user.js
+// @updateURL    http://lazarec.fr/downloader_for_claroline.user.js
+// ==/UserScript==
+
+
+(function () {
+    'use strict';
+
+    if (document.getElementsByClassName('fa-download').length == 0)
+    {
+        let download = window.location.pathname.split('/')[4];
+        document.getElementsByClassName('page-header')[0].innerHTML += '        <nav class="page-actions">\n' +
+            '        <a class="btn page-action-btn" id="downbutton" href="/clarolinepdfplayerbundle/pdf/' + download + '#downloaded">\n' +
+            '        <span class="action-icon fa fa-download"></span>\n' +
+            '    <span class="sr-only">Télécharger</span>\n' +
+            '</a>\n' +
+            '<a id="copy-permalink-to-clipboard" class="btn page-action-btn" href="/clarolinepdfplayerbundle/pdf/' + download + '#downloaded">\n' +
+            '<span class="action-icon fa fa-clipboard"></span>\n' +
+            '<span class="sr-only">Copier le permalien dans le presse-papiers</span>\n' +
+            '</a>\n' +
+            '</nav>'
+
+    }
+
+
+})();
